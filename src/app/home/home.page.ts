b362e40d1091fc1a17dbee205cfbd3e9b0d7c755
alejandroantivero <alejandro.antivero@gmail.com>
2019-03-10 11:18:32 -0300

    plugin de camara

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  currentImage:any;
+
+  constructor(private camera:Camera){}
+
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue:" + err);
+    });
+  }
 }
